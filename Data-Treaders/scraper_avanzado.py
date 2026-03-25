5b7107ce2bf3fdbfebc619e1525e9d81fbb5edc0
Juan Egana <juan.egana@alumnos.ucn.cl>
2026-03-24 22:56:14 -0300

    Semana 2: Scraper funcional para [Data Treaders]

---
+++ Data-Treaders/scraper_avanzado.py
@@ -0,0 +1,33 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "57203c99-2828-4e81-96d0-6b2bee7eb0de",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.6"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
